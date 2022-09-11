b1e98c0fd4db63edade593d0a4fb66f503144735
Matthew L <mattlonis@gmail.com>
2022-09-11 02:25:52 -0400

    Added default afk

---
+++ api-afk-arena/src/main/java/com/mrlonis/afkarena/ApiAfkArenaApplication.java
@@ -0,0 +1,13 @@
+package com.mrlonis.afkarena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAfkArenaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAfkArenaApplication.class, args);
+	}
+
+}
---
+++ api-afk-arena/src/test/java/com/mrlonis/afkarena/ApiAfkArenaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrlonis.afkarena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAfkArenaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
