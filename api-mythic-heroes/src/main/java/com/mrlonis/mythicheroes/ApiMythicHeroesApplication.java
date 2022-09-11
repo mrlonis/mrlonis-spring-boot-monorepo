79093997d7b0dbefdc09708a16def6483d728617
Matthew L <mattlonis@gmail.com>
2022-09-11 00:35:05 -0400

    Initial Commit

---
+++ api-mythic-heroes/src/main/java/com/mrlonis/mythicheroes/ApiMythicHeroesApplication.java
@@ -0,0 +1,13 @@
+package com.mrlonis.mythicheroes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMythicHeroesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiMythicHeroesApplication.class, args);
+    }
+
+}
---
+++ api-mythic-heroes/src/test/java/com/mrlonis/mythicheroes/ApiMythicHeroesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrlonis.mythicheroes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMythicHeroesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
