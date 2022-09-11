cd3867d3fdd695e3affd64c0bb18dd557975278f
Matthew L <mattlonis@gmail.com>
2022-09-11 01:39:56 -0400

    Initial  Project creation for api-genshin-impact

---
+++ api-genshin-impact/src/main/java/com/mrlonis/genshinimpact/ApiGenshinImpactApplication.java
@@ -0,0 +1,13 @@
+package com.mrlonis.genshinimpact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGenshinImpactApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGenshinImpactApplication.class, args);
+    }
+
+}
---
+++ api-genshin-impact/src/test/java/com/mrlonis/genshinimpact/ApiGenshinImpactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrlonis.genshinimpact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGenshinImpactApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
