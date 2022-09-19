59e9c66b8bd8035b83b4419d377c69128430c346
Matthew L <mattlonis@gmail.com>
2022-09-18 20:38:28 -0400

    Updating project config

--- api-mythic-heroes/src/test/java/com/mrlonis/mythicheroes/mythichero/repository/MythicHeroRepositoryH2Tests.java
+++ api-mythic-heroes/src/test/java/com/mrlonis/mythicheroes/mythichero/repository/MythicHeroRepositoryH2Tests.java
@@ -1,8 +1,18 @@
 package com.mrlonis.mythicheroes.mythichero.repository;
 
+import com.mrlonis.mythicheroes.faction.Faction;
+import com.mrlonis.mythicheroes.faction.FactionRepository;
 import com.mrlonis.mythicheroes.mythichero.MythicHero;
 import com.mrlonis.mythicheroes.mythichero.MythicHeroRepository;
 import com.mrlonis.mythicheroes.templates.repsoitory.RepositoryH2Tests;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@DataJpaTest
+@ActiveProfiles("h2")
 public class MythicHeroRepositoryH2Tests extends RepositoryH2Tests<MythicHero, MythicHeroRepository> {
 }
--- api-mythic-heroes/src/test/java/com/mrlonis/mythicheroes/templates/repsoitory/RepositoryH2Tests.java
+++ api-mythic-heroes/src/test/java/com/mrlonis/mythicheroes/templates/repsoitory/RepositoryH2Tests.java
@@ -9,10 +9,22 @@ import org.springframework.test.context.ActiveProfiles;
 
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
-@DataJpaTest
-@ActiveProfiles("h2")
+
+
+/**
+ * To use this class, you must extend it and add the following annotations:
+ * <ul>
+ *     <li>&#064;DataJpaTest</li>
+ *     <li>&#064;ActiveProfiles("h2")</li>
+ * </ul>
+ *
+ * @param <E> The Entity class to use
+ * @param <T> The PagingAndSortingRepository to use
+ * @see DataJpaTest
+ * @see ActiveProfiles
+ */
 public abstract class RepositoryH2Tests<E extends BaseEntity, T extends PagingAndSortingRepository<E, UUID>> {
 
     @Autowired
