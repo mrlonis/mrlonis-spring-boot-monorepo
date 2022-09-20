32f4a2b1b52fa7dc361f2f045853255133cc6843
Matthew L <mattlonis@gmail.com>
2022-09-19 23:10:38 -0400

    Update Class.java

--- api-afk-arena/src/main/java/com/mrlonis/afkarena/entities/Class.java
+++ api-afk-arena/src/main/java/com/mrlonis/afkarena/entities/Class.java
@@ -1,5 +1,6 @@
 package com.mrlonis.afkarena.entities;
 
+import com.mrlonis.types.BaseEntity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -8,25 +9,25 @@ import lombok.NoArgsConstructor;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.util.UUID;
 
 @Builder
 @Data
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "AfkArena_Classes")
-public class Class implements ProjectEntity {
+@Table(name = "Afk_Arena_Classes")
+public class Class implements BaseEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "ClassId")
-    private Integer classId;
+    @GeneratedValue
+    @Column(name = "id")
+    private UUID id;
 
-    @Column(name = "ClassName")
-    private String className;
+    @Column(name = "name")
+    private String name;
 
-    @Column(name = "ClassImageUrl")
-    private String classImageUrl;
+    @Column(name = "imageUrl")
+    private String imageUrl;
 }
