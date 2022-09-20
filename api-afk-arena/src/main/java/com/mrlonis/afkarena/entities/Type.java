a4ad76bda03c6fe7b953742360b5751c63121451
Matthew L <mattlonis@gmail.com>
2022-09-19 23:10:46 -0400

    Update Type.java

--- api-afk-arena/src/main/java/com/mrlonis/afkarena/entities/Type.java
+++ api-afk-arena/src/main/java/com/mrlonis/afkarena/entities/Type.java
@@ -1,5 +1,6 @@
 package com.mrlonis.afkarena.entities;
 
+import com.mrlonis.types.BaseEntity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -8,25 +9,26 @@ import lombok.NoArgsConstructor;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.io.Serializable;
+import java.util.UUID;
 
 @Builder
 @Data
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "AfkArena_Types")
-public class Type implements ProjectEntity {
+@Table(name = "Afk_Arena_Types")
+public class Type implements BaseEntity, Serializable {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "TypeId")
-    private Integer typeId;
+    @GeneratedValue
+    @Column(name = "id")
+    private UUID id;
 
-    @Column(name = "TypeName")
-    private String typeName;
+    @Column(name = "name")
+    private String name;
 
-    @Column(name = "TypeImageUrl")
-    private String typeImageUrl;
+    @Column(name = "imageUrl")
+    private String imageUrl;
 }
