857cd9f6ee538ab8f40c493b6f035a72f1d7e76a
Matthew L <mattlonis@gmail.com>
2022-09-19 23:10:40 -0400

    Update Faction.java

--- api-afk-arena/src/main/java/com/mrlonis/afkarena/entities/Faction.java
+++ api-afk-arena/src/main/java/com/mrlonis/afkarena/entities/Faction.java
@@ -1,5 +1,6 @@
 package com.mrlonis.afkarena.entities;
 
+import com.mrlonis.types.BaseEntity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -8,25 +9,25 @@ import lombok.NoArgsConstructor;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.util.UUID;
 
 @Builder
 @Data
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "AfkArena_Factions")
-public class Faction implements ProjectEntity {
+@Table(name = "Afk_Arena_Factions")
+public class Faction implements BaseEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "FactionId")
-    private Integer factionId;
+    @GeneratedValue
+    @Column(name = "id")
+    private UUID id;
 
-    @Column(name = "FactionName")
-    private String factionName;
+    @Column(name = "name")
+    private String name;
 
-    @Column(name = "FactionImageUrl")
-    private String factionImageUrl;
+    @Column(name = "imageUrl")
+    private String imageUrl;
 }
