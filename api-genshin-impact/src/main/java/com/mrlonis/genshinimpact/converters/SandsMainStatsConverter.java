4bdec57d232bbda926f8de64f5bce094156e9b27
Matthew Lonis <mattlonis@gmail.com>
2023-11-10 21:49:56 -0500

    Update SandsMainStatsConverter.java

--- api-genshin-impact/src/main/java/com/mrlonis/genshinimpact/converters/SandsMainStatsConverter.java
+++ api-genshin-impact/src/main/java/com/mrlonis/genshinimpact/converters/SandsMainStatsConverter.java
@@ -3,10 +3,12 @@ package com.mrlonis.genshinimpact.converters;
 import com.mrlonis.genshinimpact.enums.SandsMainStats;
 import jakarta.persistence.AttributeConverter;
 import jakarta.persistence.Converter;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.stream.Stream;
 
 @Converter(autoApply = true)
+@Slf4j
 public class SandsMainStatsConverter implements AttributeConverter<SandsMainStats, String> {
 
     @Override
@@ -19,6 +21,7 @@ public class SandsMainStatsConverter implements AttributeConverter<SandsMainStat
 
     @Override
     public SandsMainStats convertToEntityAttribute(String value) {
+        log.info("SandsMainStatsConverter: convertToEntityAttribute: {}", value);
         if (value == null) {
             return null;
         }
