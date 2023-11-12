9df3364b26261a3e8a0b428fb807bd04067973d8
Matthew Lonis <mattlonis@gmail.com>
2023-11-11 23:47:38 -0500

    Update ApiGenshinImpactApplication.java

--- api-genshin-impact/src/main/java/com/mrlonis/genshinimpact/ApiGenshinImpactApplication.java
+++ api-genshin-impact/src/main/java/com/mrlonis/genshinimpact/ApiGenshinImpactApplication.java
@@ -1,5 +1,6 @@
 package com.mrlonis.genshinimpact;
 
+import com.mrlonis.genshinimpact.utils.WeaponsFlywayMigrationCreation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,14 +8,6 @@ import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
@@ -35,25 +28,7 @@ public class ApiGenshinImpactApplication implements WebMvcConfigurer {
     public static void flywayMigrationCreation() {
         log.info("ApiGenshinImpactApplication: flywayMigrationCreation(): Starting...");
 
-        List<List<String>> records = new ArrayList<>();
-        ClassLoader classloader = Thread.currentThread().getContextClassLoader();
-        try (InputStream is = classloader.getResourceAsStream("csv/weapons.csv")) {
-            if (is == null) {
-                log.error("ApiGenshinImpactApplication: flywayMigrationCreation(): Error: File not found!");
-                throw new RuntimeException("File not found!");
-            }
-
-            BufferedReader br = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
-            String line;
-            while ((line = br.readLine()) != null) {
-                String[] values = line.split(",");
-                records.add(Arrays.asList(values));
-            }
-            log.info("ApiGenshinImpactApplication: flywayMigrationCreation(): Records: " + records);
-        } catch (IOException e) {
-            log.error("ApiGenshinImpactApplication: flywayMigrationCreation(): Error: " + e.getMessage());
-            throw new RuntimeException(e);
-        }
+        WeaponsFlywayMigrationCreation.createWeaponsFlywayMigration();
 
         log.info("ApiGenshinImpactApplication: flywayMigrationCreation(): Finished!");
     }
