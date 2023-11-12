aa14419f5cfa4e991766a0c3576dd8487bbd5d3a
Matthew Lonis <mattlonis@gmail.com>
2023-11-12 01:43:55 -0500

    Update WeaponsFlywayMigrationCreation.java

--- api-genshin-impact/src/main/java/com/mrlonis/genshinimpact/utils/WeaponsFlywayMigrationCreation.java
+++ api-genshin-impact/src/main/java/com/mrlonis/genshinimpact/utils/WeaponsFlywayMigrationCreation.java
@@ -1,5 +1,10 @@
 package com.mrlonis.genshinimpact.utils;
 
+import com.mrlonis.genshinimpact.converters.WeaponSecondaryStatsConverter;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
+import com.opencsv.enums.CSVReaderNullFieldIndicator;
+import com.opencsv.exceptions.CsvException;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.BufferedReader;
@@ -9,7 +14,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -19,39 +23,112 @@ public class WeaponsFlywayMigrationCreation {
     }
 
     public static void createWeaponsFlywayMigration() {
-        log.info("WeaponsFlywayMigrationCreation: createWeaponsFlywayMigration(): Starting...");
+        log.info("createWeaponsFlywayMigration(): Starting...");
 
-        List<List<String>> records = new ArrayList<>();
+        List<String[]> csvFile = parseCsvFile();
+        String fileContents = createFileContents(csvFile);
+        writeMigrationFile(fileContents);
+
+        log.info("createWeaponsFlywayMigration(): Finished!");
+    }
+
+    private static List<String[]> parseCsvFile() {
         ClassLoader classloader = Thread.currentThread().getContextClassLoader();
-        try (InputStream is = classloader.getResourceAsStream("csv/weapons.csv")) {
-            if (is == null) {
-                log.error("WeaponsFlywayMigrationCreation: createWeaponsFlywayMigration(): Error: File not found!");
+        try (InputStream inputStream = classloader.getResourceAsStream("csv/weapons.csv")) {
+            if (inputStream == null) {
+                log.error("parseCsvFile(): Error: File not found!");
                 throw new RuntimeException("File not found!");
             }
 
-            BufferedReader br = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
-            String line;
-            while ((line = br.readLine()) != null) {
-                String[] values = line.split(",");
-                records.add(Arrays.asList(values));
-            }
-            log.info("WeaponsFlywayMigrationCreation: createWeaponsFlywayMigration(): Records: " + records);
+            return readCsvFile(new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8)));
         } catch (IOException e) {
-            log.error("WeaponsFlywayMigrationCreation: createWeaponsFlywayMigration(): Error: " + e.getMessage());
+            log.error("parseCsvFile(): IOException: " + e.getMessage());
             throw new RuntimeException(e);
         }
+    }
 
+    private static List<String[]> readCsvFile(BufferedReader bufferedReader) {
+        try (CSVReader csvReader = new CSVReaderBuilder(bufferedReader).withSkipLines(1)
+                                                                       .withFieldAsNull(
+                                                                               CSVReaderNullFieldIndicator.EMPTY_SEPARATORS)
+                                                                       .build()) {
+            return csvReader.readAll();
+        } catch (CsvException e) {
+            log.error("readCsvFile(): CsvException: " + e.getMessage());
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            log.error("readCsvFile(): IOException: " + e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static String createFileContents(List<String[]> csvFile) {
+        StringBuffer fileContents = new StringBuffer();
+        fileContents.append("""
+                            INSERT INTO Weapons (
+                                    name,
+                                    imageUrl,
+                                    rarity,
+                                    weaponType,
+                                    secondaryStat,
+                                    weaponAffix,
+                                    affixDescription,
+                                    description
+                                )
+                            VALUES (
+                            """);
+        for (int i = 0; i < csvFile.size(); i++) {
+            String[] row = csvFile.get(i);
+            if (row.length != 8) {
+                String errorMessage =
+                        String.format("createFileContents(): Row %s size is not 8 and was instead %s! row: %s", i,
+                                      row.length, Arrays.toString(row));
+                log.error(errorMessage);
+                throw new RuntimeException(errorMessage);
+            }
+
+            fileContents.append(row[0] != null ? String.format("        '%s', -- name%n", row[0].replace("'", "''")) :
+                                "        NULL, -- name\n");
+            fileContents.append(
+                    row[1] != null ? String.format("        '%s', -- imageUrl%n", row[1].replace("'", "''")) :
+                    "        NULL, -- imageUrl\n");
+            fileContents.append(
+                    row[2] != null ? String.format("        %s, -- rarity%n", row[2]) : "        NULL, -- rarity\n");
+            fileContents.append(
+                    row[3] != null ? String.format("        '%s', -- weaponType%n", row[3].replace("'", "''")) :
+                    "        NULL, -- weaponType\n");
+            fileContents.append(
+                    row[4] != null ? String.format("        '%s', -- secondaryStat%n", WeaponSecondaryStatsConverter.convertToEntityAttributeFromCsvValue(row[4].replace("'", "''")).getValue()) :
+                    "        NULL, -- secondaryStat\n");
+            fileContents.append(
+                    row[5] != null ? String.format("        '%s', -- weaponAffix%n", row[5].replace("'", "''")) :
+                    "        NULL, -- weaponAffix\n");
+            fileContents.append(
+                    row[6] != null ? String.format("        '%s', -- affixDescription%n", row[6].replace("'", "''")) :
+                    "        NULL, -- affixDescription\n");
+            fileContents.append(
+                    row[7] != null ? String.format("        '%s' -- description%n", row[7].replace("'", "''")) :
+                    "        NULL -- description\n");
+            fileContents.append(")");
+            if (i != csvFile.size() - 1) {
+                fileContents.append(",\n(");
+            } else {
+                fileContents.append(";");
+            }
+        }
+        return fileContents.toString();
+    }
+
+    private static void writeMigrationFile(String fileContents) {
         try {
-            String str = "Hello";
-            BufferedWriter writer = new BufferedWriter(new FileWriter("src/main/resources/db/migration/h2/test.txt"));
-            writer.write(str);
+            BufferedWriter writer = new BufferedWriter(
+                    new FileWriter("src/main/resources/db/migration/h2/V1_1_1__Create_Weapons_Data.sql"));
+            writer.write(fileContents);
 
             writer.close();
         } catch (IOException e) {
-            log.error("WeaponsFlywayMigrationCreation: createWeaponsFlywayMigration(): Error: " + e.getMessage());
+            log.error("writeMigrationFile(): Error: " + e.getMessage());
             throw new RuntimeException(e);
         }
-
-        log.info("WeaponsFlywayMigrationCreation: createWeaponsFlywayMigration(): Finished!");
     }
 }
