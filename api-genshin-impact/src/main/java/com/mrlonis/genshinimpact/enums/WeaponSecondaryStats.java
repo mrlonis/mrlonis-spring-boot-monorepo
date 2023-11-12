7129a3560df286e4a10502329dc92c9e135aba5b
Matthew Lonis <mattlonis@gmail.com>
2023-11-12 01:43:46 -0500

    Create WeaponSecondaryStats.java

---
+++ api-genshin-impact/src/main/java/com/mrlonis/genshinimpact/enums/WeaponSecondaryStats.java
@@ -0,0 +1,23 @@
+package com.mrlonis.genshinimpact.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum WeaponSecondaryStats {
+    HEALTH_PERCENT("HP%", "HP%"),
+    ATTACK_PERCENT("ATK%", "Atk%"),
+    DEFENSE_PERCENT("DEF%", "Def%"),
+    ENERGY_RECHARGE("Energy Recharge", "ER%"),
+    ELEMENTAL_MASTERY("Elemental Mastery", "EM"),
+    CRITICAL_RATE("CRIT Rate", "CrR%"),
+    CRITICAL_DAMAGE("CRIT DMG", "CrD%"),
+    PHYSICAL_DAMAGE_BONUS("Physical DMG Bonus", "Phys%");
+
+    private final String value;
+    private final String csvValue;
+
+    private WeaponSecondaryStats(String label, String csvValue) {
+        this.value = label;
+        this.csvValue = csvValue;
+    }
+}
