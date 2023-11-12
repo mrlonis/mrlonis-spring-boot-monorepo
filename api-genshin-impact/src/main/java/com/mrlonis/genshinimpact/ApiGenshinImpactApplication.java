120ad44cd52abbe03b5000c4e7105ecada56651a
Matthew Lonis <mattlonis@gmail.com>
2023-11-11 23:26:48 -0500

    Update ApiGenshinImpactApplication.java

--- api-genshin-impact/src/main/java/com/mrlonis/genshinimpact/ApiGenshinImpactApplication.java
+++ api-genshin-impact/src/main/java/com/mrlonis/genshinimpact/ApiGenshinImpactApplication.java
@@ -1,5 +1,6 @@
 package com.mrlonis.genshinimpact;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.CacheControl;
@@ -9,18 +10,21 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
+@Slf4j
 public class ApiGenshinImpactApplication implements WebMvcConfigurer {
-
     public static void main(String[] args) {
+        flywayMigrationCreation();
         SpringApplication.run(ApiGenshinImpactApplication.class, args);
     }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-
         registry.addResourceHandler("/api/images/**")
                 .addResourceLocations("classpath:/images/")
                 .setCacheControl(CacheControl.maxAge(2, TimeUnit.HOURS).cachePublic());
     }
 
+    public static void flywayMigrationCreation() {
+        log.info("ApiGenshinImpactApplication: flywayMigrationCreation()");
+    }
 }
