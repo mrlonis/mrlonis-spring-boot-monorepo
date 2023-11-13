6c73a0479387d382bc3c5ac7ce106007ab46e10d
Matthew Lonis <mattlonis@gmail.com>
2023-11-12 22:32:06 -0500

    Update WeaponSecondaryStatsConverter.java

--- api-genshin-impact/src/main/java/com/mrlonis/genshinimpact/converters/WeaponSecondaryStatsConverter.java
+++ api-genshin-impact/src/main/java/com/mrlonis/genshinimpact/converters/WeaponSecondaryStatsConverter.java
@@ -3,13 +3,22 @@ package com.mrlonis.genshinimpact.converters;
 import com.mrlonis.genshinimpact.enums.WeaponSecondaryStats;
 import jakarta.persistence.AttributeConverter;
 import jakarta.persistence.Converter;
-import lombok.extern.slf4j.Slf4j;
 
 import java.util.stream.Stream;
 
 @Converter(autoApply = true)
-@Slf4j
-public class WeaponSecondaryStatsConverter  implements AttributeConverter<WeaponSecondaryStats, String> {
+public class WeaponSecondaryStatsConverter implements AttributeConverter<WeaponSecondaryStats, String> {
+
+    public static WeaponSecondaryStats convertToEntityAttributeFromCsvValue(String csvValue) {
+        if (csvValue == null) {
+            return null;
+        }
+
+        return Stream.of(WeaponSecondaryStats.values())
+                     .filter(c -> c.getCsvValue().equals(csvValue))
+                     .findFirst()
+                     .orElse(null);
+    }
 
     @Override
     public String convertToDatabaseColumn(WeaponSecondaryStats enumValue) {
@@ -21,24 +30,12 @@ public class WeaponSecondaryStatsConverter  implements AttributeConverter<Weapon
 
     @Override
     public WeaponSecondaryStats convertToEntityAttribute(String value) {
-        log.info("convertToEntityAttribute: {}", value);
         if (value == null) {
             return null;
         }
 
         return Stream.of(WeaponSecondaryStats.values())
                      .filter(c -> c.getValue().equals(value))
-                     .findFirst()
-                     .orElse(null);
-    }
-
-    public static WeaponSecondaryStats convertToEntityAttributeFromCsvValue(String csvValue) {
-        if (csvValue == null) {
-            return null;
-        }
-
-        return Stream.of(WeaponSecondaryStats.values())
-                     .filter(c -> c.getCsvValue().equals(csvValue))
                      .findFirst()
                      .orElse(null);
     }
