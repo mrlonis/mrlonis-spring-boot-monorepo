5bfc65c77e35baaf4f15d8d777d21a6fb9686cb9
Matthew Lonis <mattlonis@gmail.com>
2023-11-12 22:31:36 -0500

    Update PaginatedResponse.java

--- lib-mrlonis-testing/src/main/java/com/mrlonis/testing/PaginatedResponse.java
+++ lib-mrlonis-testing/src/main/java/com/mrlonis/testing/PaginatedResponse.java
@@ -13,11 +13,16 @@ import java.util.List;
 
 public class PaginatedResponse<T extends IBaseEntity> extends PageImpl<T> {
     @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
-    public PaginatedResponse(@JsonProperty("content") List<T> content, @JsonProperty("number") int number,
-                             @JsonProperty("size") int size, @JsonProperty("totalElements") Long totalElements,
-                             @JsonProperty("pageable") JsonNode pageable, @JsonProperty("last") boolean last,
-                             @JsonProperty("totalPages") int totalPages, @JsonProperty("sort") JsonNode sort,
-                             @JsonProperty("first") boolean first, @JsonProperty("empty") boolean empty) {
+    public PaginatedResponse(@JsonProperty("content") List<T> content,
+                             @JsonProperty("number") int number,
+                             @JsonProperty("size") int size,
+                             @JsonProperty("totalElements") Long totalElements,
+                             @JsonProperty("pageable") JsonNode pageable,
+                             @JsonProperty("last") boolean last,
+                             @JsonProperty("totalPages") int totalPages,
+                             @JsonProperty("sort") JsonNode sort,
+                             @JsonProperty("first") boolean first,
+                             @JsonProperty("empty") boolean empty) {
         super(content, PageRequest.of(number, size), totalElements);
     }
 
