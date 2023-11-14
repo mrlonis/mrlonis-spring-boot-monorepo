8a19045c364faa965f884a809e48ce6db3e51e49
Matthew Lonis <mattlonis@gmail.com>
2023-11-13 20:08:58 -0500

    Update CharactersFlywayMigrationCreation.java

--- api-genshin-impact/src/main/java/com/mrlonis/genshinimpact/utils/CharactersFlywayMigrationCreation.java
+++ api-genshin-impact/src/main/java/com/mrlonis/genshinimpact/utils/CharactersFlywayMigrationCreation.java
@@ -73,7 +73,7 @@ public class CharactersFlywayMigrationCreation {
                                     weaponType,
                                     sandsStatOne,
                                     sandsStatTwo,
-                                    sansStatThree
+                                    sandsStatThree,
                                     gobletStatOne,
                                     gobletStatTwo,
                                     gobletStatThree,
