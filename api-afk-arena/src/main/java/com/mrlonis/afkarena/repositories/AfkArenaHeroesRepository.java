686e2c85b4fc76faffa4654521828f90086fc084
Matthew Lonis <mattlonis@gmail.com>
2023-11-14 23:44:24 -0500

    Update AfkArenaHeroesRepository.java

--- api-afk-arena/src/main/java/com/mrlonis/afkarena/repositories/AfkArenaHeroesRepository.java
+++ api-afk-arena/src/main/java/com/mrlonis/afkarena/repositories/AfkArenaHeroesRepository.java
@@ -5,8 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
+import java.util.UUID;
 
 @RepositoryRestResource(collectionResourceRel = "afkArenaHeroes", path = "afkArenaHeroes")
-public interface AfkArenaHeroesRepository extends JpaRepository<AfkArenaHeroes, Integer> {
+public interface AfkArenaHeroesRepository extends JpaRepository<AfkArenaHeroes, UUID> {
     List<AfkArenaHeroes> findAll();
 }
