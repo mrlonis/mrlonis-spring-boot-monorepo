125b3d41831a8692239596bc09092fccf9408136
Matthew Lonis <mattlonis@gmail.com>
2024-02-01 00:00:57 -0800

    Adding spring-cloud-gateway

---
+++ gateway/src/test/java/com/mrlonis/gateway/GatewayApplicationTests.java
@@ -0,0 +1,14 @@
+package com.mrlonis.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class GatewayApplicationTests {
+    @Test
+    void contextLoads() {
+        assertTrue(true);
+    }
+}
