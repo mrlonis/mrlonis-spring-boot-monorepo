50e32a2b5766524a62e8b0039e8a5695037c399c
Matthew Lonis <mattlonis@gmail.com>
2025-01-10 22:52:42 -0500

    Fixing genshin impact api

--- test.sh
+++ test.sh
@@ -1,2 +1,19 @@
 #!/bin/sh
+npm install -g newman
 ./gradlew build
+./gradlew :api-genshin-impact:bootRun --args='--spring.profiles.active=local-h2' &
+echo $! >genshin-impact.pid
+sleep 15
+newman run ./api-genshin-impact/newman/api-genshin-impact.postman_collection.json -e ./api-genshin-impact/newman/api-genshin-impact-local.postman_environment.json
+if [ -f genshin-impact.pid ]; then
+    kill $(cat genshin-impact.pid) || echo "Process already terminated"
+    rm genshin-impact.pid
+fi
+./gradlew :api-honkai-star-rail:bootRun --args='--spring.profiles.active=local-h2' &
+echo $! >honkai-star-rail.pid
+sleep 15
+newman run ./api-honkai-star-rail/newman/api-honkai-star-rail.postman_collection.json -e ./api-honkai-star-rail/newman/api-honkai-star-rail-local.postman_environment.json
+if [ -f honkai-star-rail.pid ]; then
+    kill $(cat honkai-star-rail.pid) || echo "Process already terminated"
+    rm honkai-star-rail.pid
+fi
